b29fbf5b8546ea313fcb7d5956755c4006d07d60
asrorbekh <asrorbek0325@gmail.com>
2024-02-26 11:37:05 +0500

    Changed naming for service

--- app/routes/product/product.route.js
+++ app/routes/product/product.route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../../controllers/product.controller');
 const authMiddleware = require('../../middleware/auth.middleware');
-const upload = require('../../services/multerUpload');
+const upload = require('../../services/multer.upload.service');
 
 router.post('/', authMiddleware.authenticateUser, upload.array('images'), productController.createProduct);
 router.put('/:id', authMiddleware.authenticateUser, upload.array('images'), productController.updateProduct);
